6ad2f6f98a54ddf4a55360fd703948b3f6e9f995
Eduardo Gutierrez <egutierrez@zipcar.com>
2012-09-03 18:57:54 -0400

    Remove puts statements from spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,4 @@ require 'rspec-puppet'
 RSpec.configure do |c|
   c.module_path = File.expand_path(File.join(__FILE__, '..', 'fixtures', 'modules'))
   c.manifest_dir = File.expand_path(File.join(__FILE__, '..', 'fixtures', 'manifests'))
-  puts c.module_path
-  puts c.manifest_dir
 end
